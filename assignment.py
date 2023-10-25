ec05402d99ae3deb6a8899063d6ae64a36f8913f
kieranHH <144241789+kieranHH@users.noreply.github.com>
2023-10-25 09:16:25 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,16 @@
+inport math
+# Input the diameter of the circle
+diameter = float(input("Enter the diameter of the circle: "))
 
+# Calculate the radius (r) from the diameter
+radius = diameter / 2
+
+# Calculate the area
+area = math.pi * (radius ** 2)
+
+# Calculate the circumference
+circumference = math.pi * diameter
+
+# Print the results
+print(f"Area of the circle: {area:.2f}")
+print(f"Circumference of the circle: {circumference:.2f}")
